744d6e78d47e06b4dbd0635899de3980165dfb07
Ewooral <boahen.cos@gmail.com>
2022-04-16 02:29:43 +0100

    Starting Machine  learning

--- Numpy/tutorials.py
+++ Numpy/tutorials.py
@@ -8,21 +8,23 @@ _1dnew = np.array(_1d, dtype=int)
 print(_1dnew)
 
 _2d = [[1, 2, 3], [4, 5, 6]]
-_2dnew = np.array(_2d, dtype=float)
+_2dnew = np.array(_2d, dtype=int)
 print(_2dnew)
 
 _3d = [[11, 22, 13], [14, 5, 16]]
-_3dnew = np.array(_3d, dtype=float)
+_3dnew = np.array(_3d, dtype=int)
 print(_3dnew)
 
 # Arithmetical Operations
-_sub = _3dnew - _2dnew
-print("Subtraction: ", _sub)
 
+print("subtract: ", np.subtract(_3dnew, _2dnew))
+print("add: ", np.add(_3dnew, _2dnew))
+print("multiply: ", np.multiply(_3dnew, _2dnew))
+# print("divide: ", np.divide(_3dnew, _2dnew))
+
+# comparison
+print(_2dnew > 4)
 
 # shape of array n x n, length
 print(_2dnew.shape, len(_2dnew)) # 
 
-_save = np.save("numpies", _2dnew)
-
-print(_save)
