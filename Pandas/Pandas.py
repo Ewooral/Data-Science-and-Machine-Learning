f9a4a534663201f5c93c1ae3931ad511dd33ab2d
Ewooral <boahen.cos@gmail.com>
2022-04-21 03:07:26 +0100

    Pandas in Machine learning

--- Pandas/Pandas.py
+++ Pandas/Pandas.py
@@ -1,5 +1,31 @@
+from pickle import FALSE
 import pandas as pd
 
+"""
+SERIES
+A one-dimensional labeled array capable of holding any data type
+"""
 
-s = pd.Series([3, -5, 7, 4], index=['a', 'b', 'c', 'd'])
+s = pd.Series([3, -5, 7, 4], index=['a', False, 'c', .34])
 print(s)
+
+
+
+print("...........................................")
+'''
+DataFrame
+
+A two-dimensional labeled data structure with columns of potentially
+different types
+'''
+data = {
+
+    'Country': ['Belgium', 'India', 'Ghana', 'Brazil'],
+    'Capital': ['Brussels', 'New Delhi', 'Accra', 'Brasilia'],
+    'Population':[145000000, 1300000000, 31000000, 207000000]
+
+}
+
+data_frame = pd.DataFrame(data, columns=['Country', 'Capital', 'Population'])
+
+print(data_frame)
